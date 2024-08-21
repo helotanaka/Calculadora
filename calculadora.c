a189cdc025f0ae86f959a734307606eafafe100d
Tanaka <130470569+helotanaka@users.noreply.github.com>
2024-08-21 18:16:27 -0300

    first commit

---
+++ calculadora.c
@@ -0,0 +1,303 @@
+// Heloísa Tanaka Fernandes - htf - 21/08/2024 16:02 - 18:14
+//Primeira questão e base na main
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void primeiro(int numero);
+
+int main(void) {
+  int escolha;
+  float numero;
+  printf("Digite o número da opção: \n1 - Base 10 para 2, 8, 16 e BCD\n2 - Base 10 para 16 com complemento a 2\n3 - Decimal para Float e Double\n");
+  scanf("%d", &escolha);
+
+  printf("\nDigite o numero para operação: \n");
+  scanf("%f", &numero);
+  
+  if(escolha == 1){
+    primeiro(numero);
+  }
+}
+
+void primeiro(int numero){
+
+  printf("\n1 OPÇÃO-----------\n\n");
+  
+  double resultado;
+  int resto,  num = numero;
+  char str1[1000] = "", str2[1000] = "";
+
+//Base 2
+  printf("\nBase 2: \n\n");
+  while(1){
+    
+    resultado = num/2;
+    resto = num%2;
+    printf("%d / 2 = %.0f, resto = %d\n", num, resultado, resto);
+    num = resultado;
+
+    sprintf(str2, "%d", resto);
+    strcat(str2, str1);
+    strcpy(str1, str2);
+    strcpy(str2, "");
+    
+    if (num <= 1) {
+      sprintf(str2, "%d", num);
+      strcat(str2, str1);
+      strcpy(str1, str2);
+      break;
+    }
+  }
+  printf("Binário: %s\n\n", str1);
+
+//Base 8
+
+  num = numero;
+  strcpy(str1, "");
+  strcpy(str2, "");
+
+  printf("Base 8: \n\n");
+  while(1){
+
+    resultado = num/8;
+    resto = num%8;
+    printf("%d / 8 = %.0f, resto = %d\n", num, resultado, resto);
+    num = resultado;
+
+    sprintf(str2, "%d", resto);
+    strcat(str2, str1);
+    strcpy(str1, str2);
+    strcpy(str2, "");
+
+    if (num <= 7) {
+      sprintf(str2, "%d", num);
+      strcat(str2, str1);
+      strcpy(str1, str2);
+      break;
+    }
+  }
+  printf("Octal: %s\n\n", str1);
+  
+//Base 16
+  
+  num = numero;
+  strcpy(str1, "");
+  strcpy(str2, "");
+  
+  printf("Base 16: \n\n");
+  while(1){
+
+    resultado = num/16;
+    resto = num%16;
+    printf("%d / 16 = %.0f, resto = %d\n", num, resultado, resto);
+      num = resultado;
+
+    sprintf(str2, "%d", resto);
+    strcat(str2, str1);
+    strcpy(str1, str2);
+    strcpy(str2, "");
+
+    if (num <= 15) {
+      sprintf(str2, "%d", num);
+      strcat(str2, str1);
+      strcpy(str1, str2);
+      break;
+    }
+  }
+  printf("Hexadecial: %s\n\n", str1);
+
+//BCD
+  num = numero;
+  strcpy(str1, "");
+  strcpy(str2, "");
+  char str3[1000] = "", str4[1000] = "";
+  int c = 0;
+
+  printf("BCD: \n\n");
+
+  while(1){
+    c++;
+    resto = num%10;
+    switch (resto){
+      case 0:
+        if (c == 1){
+          strcpy(str3, "0 = 0000.");
+          strcpy(str2, "0000");
+        } else {
+          strcpy(str3, "0 = 0000, ");
+          strcpy(str2, "0000 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+        
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+      
+      case 1:
+        if (c == 1){
+          strcpy(str3, "1 = 0001.");
+          strcpy(str2, "0001.");
+        } else {
+          strcpy(str3, "1 = 0001, ");
+          strcpy(str2, "0001 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+      
+      case 2:
+        if (c == 1){
+          strcpy(str3, "2 = 0010.");
+          strcpy(str2, "0010.");
+        } else {
+          strcpy(str3, "2 = 0010, ");
+          strcpy(str2, "0010 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+      
+      case 3:
+        if (c == 1){
+          strcpy(str3, "3 = 0011.");
+          strcpy(str2, "0011.");
+        } else {
+          strcpy(str3, "3 = 0011, ");
+          strcpy(str2, "0011 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+      
+      case 4:
+        if (c == 1){
+          strcpy(str3, "4 = 0100.");
+          strcpy(str2, "0100.");
+        } else {
+          strcpy(str3, "4 = 0100, ");
+          strcpy(str2, "0100 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+      
+      case 5:
+        if (c == 1){
+          strcpy(str3, "5 = 0101.");
+          strcpy(str2, "0101.");
+        } else {
+          strcpy(str3, "5 = 0101, ");
+          strcpy(str2, "0101 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+  
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+
+      case 6:
+        if (c == 1){
+          strcpy(str3, "6 = 0110.");
+          strcpy(str2, "0110.");
+        } else {
+          strcpy(str3, "6 = 0110, ");
+          strcpy(str2, "0110 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+  
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+
+      case 7:
+        if (c == 1){
+          strcpy(str3, "7 = 0111.");
+          strcpy(str2, "0111.");
+        } else {
+          strcpy(str3, "7 = 0111, ");
+          strcpy(str2, "0111 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+  
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+
+      case 8:
+        if (c == 1){
+          strcpy(str3, "8 = 1000.");
+          strcpy(str2, "1000.");
+        } else {
+          strcpy(str3, "8 = 1000, ");
+          strcpy(str2, "1000 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+  
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+
+      case 9:
+        if (c == 1){
+          strcpy(str3, "9 = 1001.");
+          strcpy(str2, "1001.");
+        } else {
+          strcpy(str3, "9 = 1001, ");
+          strcpy(str2, "1001 ");
+        }
+        strcat(str3, str4);
+        strcpy(str4, str3);
+        strcpy(str3, "");
+  
+        strcat(str2, str1);
+        strcpy(str1, str2);
+        strcpy(str2, "");
+        break;
+    }
+    if (num <= 9){
+      break;
+    }
+    num = num/10;
+  }
+
+  printf("%s\n\n", str4);
+
+  printf("BCD: %s\n\n", str1);
+}
